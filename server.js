6f7b9429dc4355bd6fe265d32d7275b092d66fe9
tdm <55875354+taraDM23@users.noreply.github.com>
2020-01-22 13:06:47 +1100

    added first prompts

--- server.js
+++ server.js
@@ -14,3 +14,32 @@ connection.connect(function (err) {
   if (err) throw err;
   start();
 });
+
+// function which prompts the user for what action they should take
+function start() {
+  inquirer
+    .prompt({
+      name: "getPostPut",
+      type: "list",
+      message: "Would you like to View, add or Update an employee's Information?",
+      choices: ["Add New", "Update", "View", "Delete", "Exit"]
+    })
+    .then(function (answer) {
+
+      if (answer.getPostPut === "Add New") {
+        post();
+      }
+      else if (answer.getPostPut === "Update") {
+        put();
+      }
+      else if (answer.getPostPut === "View") {
+        get();
+      }
+      else if (answer.getPostPut === "Delete") {
+        destroy();
+      }
+      else {
+        connection.end();
+      }
+    });
+}
